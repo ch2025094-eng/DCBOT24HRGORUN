cc68486d660cc0d0ee0ba3dd8b53404aa68a956f
ch2025094-eng <ch2025094@mail.chsh.cy.edu.tw>
2026-04-06 16:52:10 +0800

    Update leveling.py

--- leveling.py
+++ leveling.py
@@ -1,15 +1,7 @@
 import discord
 from discord.ext import commands
-import json
 import random
-
-def load_data():
-    with open("database/levels.json", "r") as f:
-        return json.load(f)
-
-def save_data(data):
-    with open("database/levels.json", "w") as f:
-        json.dump(data, f, indent=4)
+from utils import load, save
 
 class Leveling(commands.Cog):
     def __init__(self, bot):
@@ -20,21 +12,22 @@ class Leveling(commands.Cog):
         if message.author.bot:
             return
 
-        data = load_data()
+        data = load("database/levels.json")
         user = str(message.author.id)
 
         if user not in data:
             data[user] = {"xp": 0, "level": 1}
 
-        xp_gain = random.randint(5, 15)
-        data[user]["xp"] += xp_gain
+        xp = random.randint(5, 15)
+        data[user]["xp"] += xp
 
         if data[user]["xp"] >= data[user]["level"] * 100:
             data[user]["xp"] = 0
             data[user]["level"] += 1
-            await message.channel.send(f"🎉 {message.author} 升級了！Lv.{data[user]['level']}")
+            await message.channel.send(f"🎉 {message.author} 升到 Lv.{data[user]['level']}")
 
-        save_data(data)
+        save("database/levels.json", data)
+        await self.bot.process_commands(message)
 
 async def setup(bot):
     await bot.add_cog(Leveling(bot))
