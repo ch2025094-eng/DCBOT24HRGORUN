c1744f4453fd8d9d23c152cf1559d8f907bc3a13
ch2025094-eng <ch2025094@mail.chsh.cy.edu.tw>
2026-04-06 17:08:55 +0800

    Update shop.py

--- cogs/shop.py
+++ cogs/shop.py
@@ -19,25 +19,19 @@ class Shop(commands.Cog):
         shop_data = load("database/shop.json")
         eco_data = load("database/economy.json")
         user = str(ctx.author.id)
-
         if item_name not in shop_data:
             return await ctx.send("❌ 沒有這個商品")
-
         if user not in eco_data:
             eco_data[user] = {"money": 0}
-
         price = shop_data[item_name]["price"]
         if eco_data[user]["money"] < price:
             return await ctx.send("❌ 錢不夠")
-
         eco_data[user]["money"] -= price
         save("database/economy.json", eco_data)
-
         role_id = int(shop_data[item_name]["role_id"])
         role = ctx.guild.get_role(role_id)
         if role is None:
-            return await ctx.send("❌ 找不到這個身分組 (ID 錯誤)")
-
+            return await ctx.send("❌ 找不到這個身分組 (ID錯誤)")
         try:
             await ctx.author.add_roles(role)
             await ctx.send(f"✅ 購買成功：{item_name}")
