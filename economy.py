04aaa88986f43e2debb381a7342d217f42ac5f5c
ch2025094-eng <ch2025094@mail.chsh.cy.edu.tw>
2026-04-06 16:51:57 +0800

    Update economy.py

--- economy.py
+++ economy.py
@@ -1,15 +1,7 @@
 import discord
 from discord.ext import commands
-import json
 import random
-
-def load_data():
-    with open("database/economy.json", "r") as f:
-        return json.load(f)
-
-def save_data(data):
-    with open("database/economy.json", "w") as f:
-        json.dump(data, f, indent=4)
+from utils import load, save
 
 class Economy(commands.Cog):
     def __init__(self, bot):
@@ -17,18 +9,18 @@ class Economy(commands.Cog):
 
     @commands.command()
     async def balance(self, ctx):
-        data = load_data()
+        data = load("database/economy.json")
         user = str(ctx.author.id)
 
         if user not in data:
             data[user] = {"money": 0}
-            save_data(data)
 
         await ctx.send(f"💰 你的錢：{data[user]['money']}")
+        save("database/economy.json", data)
 
     @commands.command()
     async def work(self, ctx):
-        data = load_data()
+        data = load("database/economy.json")
         user = str(ctx.author.id)
 
         if user not in data:
@@ -36,7 +28,7 @@ class Economy(commands.Cog):
 
         earn = random.randint(50, 150)
         data[user]["money"] += earn
-        save_data(data)
+        save("database/economy.json", data)
 
         await ctx.send(f"🧑‍💼 你賺了 {earn} 元！")
 
