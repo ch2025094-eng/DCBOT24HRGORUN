28251218575476e89af067dba31cb77755365282
ch2025094-eng <ch2025094@mail.chsh.cy.edu.tw>
2026-04-10 13:30:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 import discord
 from discord.ext import commands
-import os
 import asyncio
+import os
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-@bot.event
-async def on_ready():
-    print(f"已上線：{bot.user}")
-
 async def load_cogs():
-    import os
     for file in os.listdir("./cogs"):
         if file.endswith(".py"):
             await bot.load_extension(f"cogs.{file[:-3]}")
 
+@bot.event
+async def on_ready():
+    print(f"已登入：{bot.user}")
+
+    try:
+        synced = await bot.tree.sync()
+        print(f"已同步 {len(synced)} 個 Slash 指令")
+    except Exception as e:
+        print(e)
+
 async def main():
     async with bot:
         await load_cogs()
-        await bot.start(os.getenv("DISCORD_TOKEN"))
+        await bot.start("DISCORD_TOKEN")
 
 asyncio.run(main())
